9575ebe2d0d6207e9e99176943ec3f20ec3d3416
wondwoesn <revatoel@gmail.com>
2024-07-06 01:20:43 +0300

    perfectly handled data fetch from an api

--- 2-try_async.js
+++ 2-try_async.js
@@ -2,12 +2,24 @@
 // it still needs error handling with try...catch
 
 const url = "https://jsonplaceholder.typicode.com/users";
+// this the bare minimum code to fetch data using async/await syntax
+// handling an error is crucial in every aspect of the code
+// so use try catch in this
 
-async function getData() {
-  const response = await fetch(url); // Wait until fetch completes
-  const data = await response.json(); //wait until JSON parsing completes
+async function fetchData() {
+  const response = await fetch(url);
+  try {
+    if (!response.ok) {
+      throw new Error("response gone wrong!", response.status);
+    } else {
+      console.log(response); //unformatted response from the api call
+    }
 
-  console.log(data); //log the parsed data
+    //parse repsone to JSON  format
+    const dataObj = await response.json();
+  } catch (error) {
+    console.error("Error encounterd");
+  }
 }
 
-getData();
+fetchData();
