9465ccb5f650614cbd68c3bd60f0f2cf2306ce59
wondwoesn <revatoel@gmail.com>
2024-07-06 00:46:17 +0300

    async/await method instead of chaining then()

--- 2-try_async.js
+++ 2-try_async.js
@@ -1,2 +1,13 @@
 // using async/await instead of chaining then()
 // it still needs error handling with try...catch
+
+const url = "https://jsonplaceholder.typicode.com/users";
+
+async function getData() {
+  const response = await fetch(url); // Wait until fetch completes
+  const data = await response.json(); //wait until JSON parsing completes
+
+  console.log(data); //log the parsed data
+}
+
+getData();
