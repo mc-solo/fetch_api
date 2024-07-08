4ab58b88d70cf3d0865230666748797a11bd1fac
wondwoesn <revatoel@gmail.com>
2024-07-08 16:20:15 +0300

    add task.js

--- 2-try_async.js
+++ 2-try_async.js
@@ -1,5 +1,9 @@
 // using async/await instead of chaining then()
 // it still needs error handling with try...catch
+// the response var holds a response object that is returened from the fetch api, and we can apply lots of methods to this object like:
+// .json: parses the response into a json format
+// .text: returns the response body as text str
+// .blob: returns a blob object representing the response body...
 
 const url = "https://jsonplaceholder.typicode.com/users";
 // this the bare minimum code to fetch data using async/await syntax
@@ -7,18 +11,16 @@ const url = "https://jsonplaceholder.typicode.com/users";
 // so use try catch in this
 
 async function fetchData() {
-  const response = await fetch(url);
   try {
+    const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("response gone wrong!", response.status, response.text);
+      throw new Error("Response was not okay!");
     } else {
-      console.log(response); //unformatted response from the api call
+      const dataObj = await response.json();
+      console.log(dataObj);
     }
-
-    //parse repsone to JSON  format
-    const dataObj = await response.json();
-  } catch (error) {
-    console.error("Error encounterd", error.message);
+  } catch (err) {
+    console.warn(`Error encountred during fetch, ${err.message},`);
   }
 }
 
